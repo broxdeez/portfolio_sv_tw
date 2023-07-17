5ecc0e756ffaa4cc32c25f35d5cf7c4615ef434b
broxdeez <broxdeez@gmail.com>
2023-07-17 16:17:18 -0700

    padding hero section

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,19 +1,21 @@
 <div class="hero h-[500px] sm:mt-10">
 	<div class="hero-content text-center">
-		<div class="max-w-lg flex flex-col items-center">
+		<div class="max-w-xl flex flex-col items-center">
             <div class="block avatar mx-auto">
 				<div class="w-32 mask mask-squircle">
 					<img src="/bharath.png" alt="headshot" />
 				</div>
 			</div>
-			<h1 class="text-4xl font-bold font-secondary">I'm Bharath Mohan</h1>
-			<p class="pt-6 text-md font-primary">
+			<h1 class="text-5xl font-bold font-secondary">I'm Bharath Mohan</h1>
+			<p class="pt-6 text-lg font-primary">
                 Crafting <span class="badge badge-primary badge-outline font-bold">Full Stack</span> solutions powered by the latest in <span class="badge badge-secondary badge-outline font-bold">ML</span> 
             </p>
-            <p class="py-4 text-md font-primary">
+            <p class="py-4 text-lg font-primary">
                 I also do <span class="badge badge-primary badge-outline font-bold">Astro</span> and <span class="badge badge-secondary badge-outline font-bold">Landscape</span> photography
             </p>
+            <div class='sm:pt-4'>   
 			<button class="w-32 btn btn-neutral bg-black">About Me</button>
+        </div>
 		</div>
 	</div>
 </div>
