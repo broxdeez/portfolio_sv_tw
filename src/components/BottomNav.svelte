87069d3dac5c4d39274f33870a5f8f3ba41649ea
broxdeez <broxdeez@gmail.com>
2023-07-17 22:02:26 -0700

    added bottom navigation

---
+++ src/components/BottomNav.svelte
@@ -0,0 +1,51 @@
+<div class="btm-nav z-50">
+	<button class="text-info">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-5 w-5"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+			/></svg
+		>
+	</button>
+	<button class="text-info active">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="h-5 w-5"
+			><rect width="20" height="14" x="2" y="7" rx="2" ry="2" /><path
+				d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"
+			/></svg
+		>
+	</button>
+	<button class="text-info">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="h-5 w-5"
+			><path
+				d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"
+			/><circle cx="12" cy="13" r="3" /></svg
+		>
+	</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import Nav from "../components/Nav.svelte";
     import Hero from "../components/Hero.svelte";
+    import BottomNav from "../components/BottomNav.svelte";
     import Project from "../components/Project.svelte";
 </script>
 
 <!-- THE WHOLE THING -->
 <div class='p-8 sm:px-32 xl:max-w-7xl mx-auto'>
     <Nav/>
+    <BottomNav />
     <Hero/>
     <Project />
 </div>
