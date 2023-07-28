c5899eaa7228d707c616b0bec1e2dd3a11089d5f
broxdeez <broxdeez@gmail.com>
2023-07-27 23:08:12 -0700

    project title center on mobile and small screens

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,22 +1,25 @@
-<div id='projects'>
-	<h1 class="h1 font-secondary text-5xl mt-24">Projects</h1>
-
-	<h1 class="h1 w-fit p-4 font-secondary text-3xl mt-10 bg-cyan-500 shadow-xl shadow-cyan-500/50">
-		WEB DEV
-	</h1>
+<div id="projects">
+	<div class="flex flex-col items-center lg:items-start">
+		<h1 class="h1 font-secondary text-5xl mt-24">Projects</h1>
+		<h1 class="h1 w-fit p-4 font-secondary text-3xl mt-10 bg-cyan-500 shadow-xl shadow-cyan-500/50">
+			WEB DEV
+		</h1>
+	</div>
 
 	<!-- <div
 		class="h-[1500px] md:h-[400px] mt-10 sm:mt-24 flex flex-col items-center md:flex-row justify-center md:space-x-5 p-2 sm:p-0"
 	> -->
-	<div class='flex flex-col items-center mt-10 lg:flex-row lg:space-x-6'>
+	<div class="flex flex-col items-center mt-10 lg:flex-row lg:space-x-6">
 		<div class="card w-96 bg-base-100 shadow-xl mb-10">
 			<figure><img class="object-cover h-60 w-full" src="project1.png" alt="Shoes" /></figure>
 			<div class="card-body">
 				<h2 class="card-title">Only Recipe</h2>
 				<p>If a dog chews shoes whose shoes does he choose?</p>
 				<div class="card-actions justify-start space-x-2">
-					<a href='https://github.com/broxdeez/svelte-recipe' class="btn btn-outline btn-primary">Github</a>
-					<a href='https://broxdeez-recipe.vercel.app/' class="btn btn-primary">Live Demo</a>
+					<a href="https://github.com/broxdeez/svelte-recipe" class="btn btn-outline btn-primary"
+						>Github</a
+					>
+					<a href="https://broxdeez-recipe.vercel.app/" class="btn btn-primary">Live Demo</a>
 				</div>
 			</div>
 		</div>
@@ -26,8 +29,11 @@
 				<h2 class="card-title">Movie DB</h2>
 				<p>If a dog chews shoes whose shoes does he choose?</p>
 				<div class="card-actions justify-start space-x-2">
-					<a href='https://github.com/broxdeez/svelte_tw_moviedb' class="btn btn-outline btn-primary">Github</a>
-					<a href='https://broxdeez-moviedb.vercel.app/' class="btn btn-primary">Live Demo</a>
+					<a
+						href="https://github.com/broxdeez/svelte_tw_moviedb"
+						class="btn btn-outline btn-primary">Github</a
+					>
+					<a href="https://broxdeez-moviedb.vercel.app/" class="btn btn-primary">Live Demo</a>
 				</div>
 			</div>
 		</div>
@@ -37,8 +43,11 @@
 				<h2 class="card-title">Skeleton Contact</h2>
 				<p>If a dog chews shoes whose shoes does he choose?</p>
 				<div class="card-actions justify-start space-x-2">
-					<a href='https://github.com/broxdeez/skeletonUI_contactForm' class="btn btn-outline btn-primary">Github</a>
-					<a href='https://broxdeez-svelte-form.vercel.app/' class="btn btn-primary">Live Demo</a>
+					<a
+						href="https://github.com/broxdeez/skeletonUI_contactForm"
+						class="btn btn-outline btn-primary">Github</a
+					>
+					<a href="https://broxdeez-svelte-form.vercel.app/" class="btn btn-primary">Live Demo</a>
 				</div>
 			</div>
 		</div>
@@ -47,11 +56,13 @@
 		<a href="/" class="link link-primary text-xl">More</a>
 	</div>
 
-	<h1 class="h1 w-fit p-4 font-secondary text-3xl mt-10 bg-cyan-500 shadow-xl shadow-cyan-500/50">
-		FULL STACK ML
-	</h1>
+	<div class="flex flex-col items-center lg:items-start">
+		<h1 class="h1 w-fit p-4 font-secondary text-3xl mt-10 bg-cyan-500 shadow-xl shadow-cyan-500/50">
+			FULL STACK ML
+		</h1>
+	</div>
 
-	<div class='flex flex-col items-center mt-10 lg:flex-row lg:space-x-6'>
+	<div class="flex flex-col items-center mt-10 lg:flex-row lg:space-x-6">
 		<div class="card w-96 bg-base-100 shadow-xl mb-10">
 			<figure><img class="object-cover h-60 w-full" src="project1.png" alt="Shoes" /></figure>
 			<div class="card-body">
